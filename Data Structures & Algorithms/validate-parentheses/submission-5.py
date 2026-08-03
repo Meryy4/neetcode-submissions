46d513b0d59177f23b5116e83336fbb6dc2fca17
Meryy4 <meryemeramdi05@gmail.com>
2026-08-03 14:00:35 +0100

    Add: validate-parentheses - submission-5

---
+++ Data Structures & Algorithms/validate-parentheses/submission-5.py
@@ -0,0 +1,20 @@
+class Solution:
+    def isValid(self, s: str) -> bool:
+        dictio = {')': '(', '}': '{', ']': '['}
+        ouvrante = ['(','{','[']
+        fermante = [')','}',']']
+        valid = True
+        stack = []
+        for element in s:
+            if element in ouvrante:
+                stack.append(element)
+            if element in fermante:
+                if not stack:
+                    valid = False
+                    break
+                elif stack.pop() == dictio[element]:
+                    continue
+                else:
+                    valid = False
+                    break
+        return (valid and not stack)
