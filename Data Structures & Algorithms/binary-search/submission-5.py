d249c88099d36f1215291c64fa6420e7011c0fa6
Meryy4 <meryemeramdi05@gmail.com>
2026-09-19 19:28:34 +0100

    Add: binary-search - submission-5

---
+++ Data Structures & Algorithms/binary-search/submission-5.py
@@ -0,0 +1,15 @@
+class Solution:
+    def search(self, nums: List[int], target: int) -> int:
+        min1=0
+        max1=len(nums)-1
+       
+            
+        while (min1<=max1):
+            mid=(min1+max1)//2
+            if nums[mid]==target:
+                return mid
+            elif nums[mid]<target:
+                min1+=1
+            else:
+                max1-=1
+        return -1
